dca0cb1a13a37739711398c960c21d2dcd2b1cfd
HakimAmraoui <hakim.amraoui@outlook.fr>
2022-12-26 18:01:19 +0100

    Day 1

---
+++ 2022/day1.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+file_path = os.path.join("Inputs/Day_1/input.txt")
+
+c = 0
+elves_calories = []
+
+with open(file_path, "r") as file:
+    for line in file:
+        if line != "\n":
+            c += int(line)
+        else :
+            elves_calories.append(c)
+            c = 0
+
+print(f'The Elf carring the most calories is {np.argmax(elves_calories)} with {elves_calories[np.argmax(elves_calories)]} calories.')
+
+t = 0
+for i in np.argsort(elves_calories)[-3:][::-1]:
+    t += elves_calories[i]
+print(f'The top three are : {np.argsort(elves_calories)[-3:][::-1]} with a total of {t} calories.')
